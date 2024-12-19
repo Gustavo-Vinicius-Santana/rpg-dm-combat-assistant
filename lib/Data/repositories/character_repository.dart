47f4002b4148af8d3015c8673b77b3545f2ec2cc
Gustavo Vinicius <gus.tec.dev@gmail.com>
2024-12-18 22:42:53 -0300

    [feature - db - complet] Adicionando metodo no repository de personagem para deletar os personagens com o id especificado

--- lib/Data/repositories/character_repository.dart
+++ lib/Data/repositories/character_repository.dart
@@ -16,6 +16,22 @@ class CharacterRepository {
     return db.query('characters', where: 'id = ?', whereArgs: [id]);
   }
 
+  Future<int> deleteRowsByIds(List<int> ids) async {
+    final db = await DB.instance.database;
+
+    if (ids.isEmpty) {
+      return 0;
+    }
+
+    final placeholders = List.filled(ids.length, '?').join(',');
+
+    return await db.delete(
+      'characters',
+      where: 'id IN ($placeholders)',
+      whereArgs: ids,
+    );
+  }
+
   Future<int> updateCharacter(int id, Map<String, dynamic> character) async {
     final db = await DB.instance.database;
     return db.update(
