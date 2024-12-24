9225cdccccacaf61685143999a557da8212698f0
Gustavo Vinicius <gus.tec.dev@gmail.com>
2024-12-24 17:14:58 -0300

    [feature - ui/logic - complet] Adicionando cor opcional nos icones.

--- lib/Ui/Components/Icons/IconsSvg.dart
+++ lib/Ui/Components/Icons/IconsSvg.dart
@@ -41,12 +41,13 @@ class AppIcons {
   static Widget combateEspadasIcon({
     double sizeheight = 30.0,
     double sizeWidth = 10.0,
+    Color? color,
   }) {
     return SvgPicture.asset(
       'assets/combate_espadas_icone.svg',
       width: sizeWidth,
       height: sizeheight,
-      color: Colors.grey,
+      color: color ?? Colors.grey,
     );
   }
 
@@ -65,48 +66,52 @@ class AppIcons {
   static Widget monstrosCabecaIcon({
     double sizeheight = 30.0,
     double sizeWidth = 10.0,
+    Color? color,
   }) {
     return SvgPicture.asset(
       'assets/monstros_cabeca_icone.svg',
       width: sizeWidth,
       height: sizeheight,
-      color: Colors.grey,
+      color: color ?? Colors.grey,
     );
   }
 
   static Widget monstrosBigCabecaIcon({
     double sizeheight = 100.0,
     double sizeWidth = 100.0,
+    Color? color,
   }) {
     return SvgPicture.asset(
       'assets/monstros_cabeca_icone.svg',
       width: sizeWidth,
       height: sizeheight,
-      color: Colors.grey,
+      color: color ?? Colors.grey,
     );
   }
 
   static Widget jogadorCabecaIcon({
     double sizeheight = 30.0,
     double sizeWidth = 10.0,
+    Color? color,
   }) {
     return SvgPicture.asset(
       'assets/jogador_cabeca_icone.svg',
       width: sizeWidth,
       height: sizeheight,
-      color: Colors.grey,
+      color: color ?? Colors.grey,
     );
   }
 
   static Widget jogadorBigCabecaIcon({
     double sizeheight = 100.0,
     double sizeWidth = 100.0,
+    Color? color,
   }) {
     return SvgPicture.asset(
       'assets/jogador_cabeca_icone.svg',
       width: sizeWidth,
       height: sizeheight,
-      color: Colors.grey,
+      color: color ?? Colors.grey,
     );
   }
 }
