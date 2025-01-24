916a5c45fc3a3e352f92e77b13ad93befcbdac35
Gustavo Vinicius <gus.tec.dev@gmail.com>
2025-01-24 16:50:29 -0300

    [feature - logic/db - incomplet] (metodos no repository) novos metodos no repository de condições

--- lib/Data/repositories/conditions_repository.dart
+++ lib/Data/repositories/conditions_repository.dart
@@ -10,4 +10,19 @@ class ConditionsRepository {
     final db = await DB.instance.database;
     return db.query('conditions', where: 'id = ?', whereArgs: [id]);
   }
+
+  Future<int> updateCondition(int id, Map<String, dynamic> condition) async {
+    final db = await DB.instance.database;
+    return db.update('conditions', condition, where: 'id = ?', whereArgs: [id]);
+  }
+
+  Future<int> deleteCondition(int id) async {
+    final db = await DB.instance.database;
+    return db.delete('conditions', where: 'id = ?', whereArgs: [id]);
+  }
+
+  Future<int> insertCondition(Map<String, dynamic> condition) async {
+    final db = await DB.instance.database;
+    return db.insert('conditions', condition);
+  }
 }
